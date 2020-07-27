c9023cef769a43d26e0c1380cee034f584d95b7a
Etoma Egot <etomaa@gmail.com>
2020-07-27 22:18:00 +0200

    Adding all at once

---
+++ Analysis.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+load("rda/murders.rda")
+
+murders %>% mutate(abb = reorder(abb, rate)) %>% 
+  ggplot(aes(abb, rate)) +
+  geom_bar(width = 0.5, stat = "identity", color = "black") +
+  coord_flip()
+
+ggsave("figs/barplot.png")#Let's save our barplot to a new directory figs
